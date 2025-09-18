0feaeb8f1b2a9b29ddf946ab7ef40e76f21a7bd7
Colin Lundy <colin@lundy.me.uk>
2025-09-18 11:57:21 +0100

    modified:   backend/src/config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,15 +1,24 @@
-const dotenv = require('dotenv');
+import dotenv from "dotenv";
 dotenv.config();
 
-module.exports = {
-  databaseUrl: process.env.DATABASE_URL,
-  auth0Domain: process.env.AUTH0_DOMAIN,
-  auth0Audience: process.env.AUTH0_AUDIENCE,
-  auth0ClientId: process.env.AUTH0_CLIENT_ID,
-  auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
-  footballDataApiKey: process.env.FOOTBALL_DATA_API_KEY,
-  emailFrom: process.env.EMAIL_FROM,
-  testMode: process.env.PAYMENTS_TEST_MODE === 'true'
+export const config = {
+  port: process.env.PORT || 5000,
+  dbUrl: process.env.DATABASE_URL,
+  auth0: {
+    domain: process.env.AUTH0_DOMAIN,
+    audience: process.env.AUTH0_AUDIENCE,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  },
+  stripe: {
+    secret: process.env.STRIPE_SECRET_KEY,
+    public: process.env.STRIPE_PUBLIC_KEY,
+  },
+  footballData: {
+    apiKey: process.env.FOOTBALL_DATA_API_KEY,
+  },
+  email: {
+    from: process.env.EMAIL_FROM,
+  },
+  testMode: process.env.PAYMENTS_TEST_MODE === "true",
 };
